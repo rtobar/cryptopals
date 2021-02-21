955e034e417188a4b590ef2c3c8a9eb1ed6a34c8
Rodrigo Tobar <rtobar@icrar.org>
2021-02-21 21:54:35 +0800

    challenge 2.2
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

---
+++ sets/2/2.cpp
@@ -0,0 +1,13 @@
+#include <cassert>
+#include <iostream>
+
+#include "cryptopals/io.h"
+#include "cryptopals/openssl.h"
+
+int main(int argc, char *argv[])
+{
+    assert(argc > 2);
+    std::cout << cryptopals::decrypt_aes128_cbc(
+                     cryptopals::read_multiline_b64_file(argv[1]), argv[2],
+                     std::string(16, '\0')) << "\n";
+}
