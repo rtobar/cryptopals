90acbf4644742ec131c7e64cd964d33ee75f796e
Rodrigo Tobar <rtobar@icrar.org>
2021-02-25 21:37:08 +0800

    small clang-format

--- sets/2/2.cpp
+++ sets/2/2.cpp
@@ -9,5 +9,6 @@ int main(int argc, char *argv[])
     assert(argc > 2);
     std::cout << cryptopals::decrypt_aes128_cbc(
                      cryptopals::read_multiline_b64_file(argv[1]), argv[2],
-                     std::string(16, '\0')) << "\n";
+                     std::string(16, '\0'))
+              << "\n";
 }
